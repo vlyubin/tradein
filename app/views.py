65a38369b8f725be9ec88a01927655cb77648fdc
vlyubin <vlyubin@gmail.com>
2014-02-21 11:30:58 -0800

    More

--- app/views.py
+++ app/views.py
@@ -1,6 +1,25 @@
-from app import app
+from flask import request, session
+from flask import render_template, make_response
+from app import app, models, db
+from flask.ext.sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+def add_product():
+  return "TODO"
+
+@app.route('/product')
+def sell():
+  return "TODO"
+
+@app.route('/sell')
+def sell():
+  return "TODO"
+
+@app.route('/search')
+def search():
+  return "TODO"
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+  return "Hello, World!"
