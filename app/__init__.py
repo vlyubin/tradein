f9b1219baa7bdc0889305edbdfd6fd1498c01949
lishid <lishid@gmail.com>
2014-02-21 15:29:43 -0800

    Added img upload. See example in /static/js/img.js

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'app/static/uploads');
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['ALLOWED_EXTENSIONS'] = ALLOWED_EXTENSIONS
+
 db = SQLAlchemy(app)
 
 from app import views, models
--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, jsonify, request
 from app import app, models, db
 from flask.ext.sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os, json
 
 @app.route('/add', methods = ['POST'])
 def add_product():
@@ -50,3 +51,28 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('500.html'), 500
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+@app.route('/img', methods=['GET', 'POST'])
+def upload_file():
+  upload_folder = app.config['UPLOAD_FOLDER'];
+  if request.method == 'POST':
+    file = request.files['image']
+    if not file or '.' not in file.filename:
+      return "ERROR";
+    extension = file.filename.rsplit('.', 1)[1]
+    if extension not in app.config['ALLOWED_EXTENSIONS']:
+      return json.dumps({"error":"File extension not allowed"})
+    filename = os.urandom(16).encode('hex') + "." + extension
+    file.save(os.path.join(upload_folder, filename))
+    return json.dumps({"file":os.path.join('/static/uploads', filename)})
+  if request.method == 'GET':
+    filename = request.form.get('filename')
+    if not filename:
+      return "ERROR";
+    file = os.path.join(upload_folder, filename);
+    if not os.path.isfile(file):
+      return "ERROR";
+    return app.send_static_file(file)
