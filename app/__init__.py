a51c651beca55eb6058a311d9929edc3e0b00cf7
vlyubin <vlyubin@gmail.com>
2014-02-21 15:57:15 -0800

    rem dup

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,12 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
-app = Flask(__name__)
-app.config.from_object('config')
-
 UPLOAD_FOLDER = os.path.join(os.getcwd(), 'app/static/uploads');
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
+
+app.config.from_object('config')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['ALLOWED_EXTENSIONS'] = ALLOWED_EXTENSIONS
 
