5660f41675f42c947c22081d5dc447d800de3967
vlyubin <vlyubin@gmail.com>
2014-02-21 13:04:16 -0800

    Beautiful error pages

--- app/views.py
+++ app/views.py
@@ -42,3 +42,11 @@ def search():
 @app.route('/index')
 def index():
     return render_template('homepage.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
