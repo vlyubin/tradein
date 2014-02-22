7eb06e1aff1f3e6914c0e03e45beadecd06c7d2d
vlyubin <vlyubin@gmail.com>
2014-02-21 21:31:37 -0800

    Someone made a mistake in js file

--- app/static/js/sell.js
+++ app/static/js/sell.js
@@ -20,7 +20,7 @@ $("#submitButton").click(function(){
     });
   } else {
     request = $.ajax({
-      url: '/edit/' + $('#productId');,
+      url: '/edit/' + $('#productId'),
       type: "post",
       data: {
         desc: $('#descf').val(),
