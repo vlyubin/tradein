f44ae1d67bec0917a00ec80aa12eecc40b730a7c
lishid <lishid@gmail.com>
2014-02-24 22:35:02 -0800

    Made image upload better.

--- app/static/js/img.js
+++ app/static/js/img.js
@@ -1,89 +1,34 @@
+var IMG;
+
 $(document).ready(function(){
-  var thumb = $("#thumb");
+  var thumbnails = $("#thumbnails");
+  var submitCallback;
+  var completeCallback;
   new AjaxUpload('imageUpload', {
     action: $('form#imageUploadForm').attr('action'),
     name: 'image',
     onSubmit: function(file, extension) {
-      thumb.addClass('loading');
+      if(submitCallback) {
+        submitCallback();
+      }
     },
     onComplete: function(file, response) {
-      thumb.removeClass('loading');
       response = JSON.parse(response);
-      thumb.attr("src", response.file);
-      console.log(response.file);
-      $("#imgLink").attr("value", response.file)
-      //TODO Save the response.file to the DB afterwards
+      if(response.error) {
+        alert("Upload error " + response.error);
+      }
+      if(completeCallback) {
+        completeCallback(response.file)
+      }
     }
   });
-});
 
-$(document).ready(function(){
-  var thumb = $("#thumb2");
-  new AjaxUpload('imageUpload2', {
-    action: $('form#imageUploadForm2').attr('action'),
-    name: 'image',
-    onSubmit: function(file, extension) {
-      thumb.addClass('loading');
+  IMG = {
+    submit: function(func) {
+      submitCallback = func;
     },
-    onComplete: function(file, response) {
-      thumb.removeClass('loading');
-      response = JSON.parse(response);
-      thumb.attr("src", response.file);
-      console.log(response.file);
-      $("#imgLink2").attr("value", response.file)
-      //TODO Save the response.file to the DB afterwards
+    complete: function(func) {
+      completeCallback = func;
     }
-  });
+  };
 });
-
-$(document).ready(function(){
-  var thumb = $("#thumb3");
-  new AjaxUpload('imageUpload3', {
-    action: $('form#imageUploadForm3').attr('action'),
-    name: 'image',
-    onSubmit: function(file, extension) {
-      thumb.addClass('loading');
-    },
-    onComplete: function(file, response) {
-      thumb.removeClass('loading');
-      response = JSON.parse(response);
-      thumb.attr("src", response.file);
-      console.log(response.file);
-      $("#imgLink3").attr("value", response.file)
-      //TODO Save the response.file to the DB afterwards
-    }
-  });
-});
-
-$(document).ready(function(){
-  var thumb = $("#thumb4");
-  new AjaxUpload('imageUpload4', {
-    action: $('form#imageUploadForm4').attr('action'),
-    name: 'image',
-    onSubmit: function(file, extension) {
-      thumb.addClass('loading');
-    },
-    onComplete: function(file, response) {
-      thumb.removeClass('loading');
-      response = JSON.parse(response);
-      thumb.attr("src", response.file);
-      console.log(response.file);
-      $("#imgLink4").attr("value", response.file)
-      //TODO Save the response.file to the DB afterwards
-    }
-  });
-});
-/*
-
-Usage:
-
-<form id="imageUploadForm" action="/img" method="post">
-  <img id="thumb" />
-  <input type="file" size="20" id="imageUpload">
-  <a class="button">Save</a>
-</form>
-
-<script src="static/js/ajaxupload.js"></script>
-<script src="static/js/img.js"></script>
-
-*/
--- app/static/js/sell.js
+++ app/static/js/sell.js
@@ -1,39 +1,73 @@
-$("#submitButton").click(function(){
+$(document).ready(function(){
+  var $thumbs = $("#thumbnails");
   var $form = $("#submitForm");
-  
-  if ($('#addedit').val() === 'add') {
-    request = $.ajax({
-      url: '/add',
-      type: "post",
-      data: {
-        desc: $('#descf').val(),
-        title: $('#titlef').val(),
-        category: $('#categoryf').val(),
-        price: $('#pricef').val(),
-        imgLink: $('#imgLink').val(),
-        imgLink2: $('#imgLink2').val(),
-        imgLink3: $('#imgLink3').val(),
-        imgLink4: $('#imgLink4').val()
-      },
+
+  $("#submitButton").click(function(){
+    var images = getImages();
+    while(images.length < 4) {
+      images.push("");
+    }
+    if ($('#addedit').val() === 'add') {
+      request = $.ajax({
+        url: '/add',
+        type: "post",
+        data: {
+          desc: $('#descf').val(),
+          title: $('#titlef').val(),
+          category: $('#categoryf').val(),
+          price: $('#pricef').val(),
+          imgLink: images[0],
+          imgLink2: images[1],
+          imgLink3: images[2],
+          imgLink4: images[3]
+        },
+      });
+    } else {
+      request = $.ajax({
+        url: '/edit/' + $('#productId').val(),
+        type: "post",
+        data: {
+          desc: $('#descf').val(),
+          title: $('#titlef').val(),
+          category: $('#categoryf').val(),
+          price: $('#pricef').val(),
+        },
+      });
+    }
+
+    request.success(function (response, textStatus, jqXHR){
+      window.location.replace(response);
     });
-  } else {
-    request = $.ajax({
-      url: '/edit/' + $('#productId').val(),
-      type: "post",
-      data: {
-        desc: $('#descf').val(),
-        title: $('#titlef').val(),
-        category: $('#categoryf').val(),
-        price: $('#pricef').val(),
-      },
+
+    request.fail(function (jqXHR, textStatus, errorThrown){
+      alert("Something went wrong :(");
     });
+  });
+
+  IMG.submit(function() {
+    console.log("Submit");
+  });
+
+  IMG.complete(function(file) {
+    console.log("Complete " + file);
+    var div = $("<div/>", {class: "image-container"});
+    var title = $("<div/>").append($("<a/>", {href: file, text: "[Full picture]", target: "_blank"}))
+    .append(" - ")
+    .append($("<a/>", {href: "#", text: "[Remove]"}).click(
+      function(event){
+        $(this).parent().parent().remove();
+        event.preventDefault();
+      }));
+    div.append(title);
+    div.append($("<img/>", {"src": file, style: "max-width:400px; max-height:300px;"}));
+    $thumbs.append(div);
+  });
+
+  function getImages() {
+    var images = new Array();
+    $thumbs.find("img").each(function() {
+      images.push($(this).attr("src"));
+    });
+    return images;
   }
-
-  request.success(function (response, textStatus, jqXHR){
-    window.location.replace(response);
-  });
-
-  request.fail(function (jqXHR, textStatus, errorThrown){
-    alert("Something went wrong :(");
-  });
 });
